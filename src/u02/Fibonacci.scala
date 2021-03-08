b550b60cc4a581b5d0ec1a069bfba61c5c79ca45
MEBoo <39593253+MEBoo@users.noreply.github.com>
2021-03-08 21:25:00 +0100

    impl Fibonacci + test

--- src/u02/Fibonacci.scala
+++ src/u02/Fibonacci.scala
@@ -1,11 +1,26 @@
-package u02
+package u02;
 
 object Fibonacci extends App{
-
+  
   def fib(n: Int):Int = n match {
     case 0 => 0
     case 1 => 1
     case _ => fib(n-2) + fib(n-1)
   }
 
+  // versione con somma in avanti di un numero fib + il precedente fib
+  def fibTail(n: Int): Int = {
+    @annotation.tailrec
+    def _fibTail(n: Int, prev: Int, current: Int): Int = {
+      if (n <= 0) {
+        current
+      } else {
+        _fibTail(n - 1, prev + current, prev)
+      }
+    }
+    _fibTail(n, 1, 0)
+  }
+
+  // versione derivante dalla scomposizione spettrale della versione matriciale
+  def fibSuperFaster(n: Int):Int =((1/Math.sqrt(5))*(Math.pow((1+Math.sqrt(5))/2,n) - Math.pow((1-Math.sqrt(5))/2,n))).toInt
 }
--- test/u02/FibonacciTest.scala
+++ test/u02/FibonacciTest.scala
@@ -3,10 +3,10 @@ package u02
 import org.junit.jupiter.api.Assertions._
 import org.junit.jupiter.api.Test
 
-
 class FibonacciTest {
 
   import u02.Fibonacci.fib
+  import u02.Fibonacci.fibTail
   
   @Test def testFibonacci(){
     assertEquals(0,fib(0))
@@ -14,6 +14,27 @@ class FibonacciTest {
     assertEquals(1,fib(2))
     assertEquals(2,fib(3))
     assertEquals(3,fib(4))
+
+    assertEquals(21,fib(8))
   }
-  
+
+  @Test def testFibonacciTail(){
+    assertEquals(0,fibTail(0))
+    assertEquals(1,fibTail(1))
+    assertEquals(1,fibTail(2))
+    assertEquals(2,fibTail(3))
+    assertEquals(3,fibTail(4))
+
+    assertEquals(21,fibTail(8))
+  }
+
+  @Test def testFibonacciSuperFaster(){
+    assertEquals(0,fibTail(0))
+    assertEquals(1,fibTail(1))
+    assertEquals(1,fibTail(2))
+    assertEquals(2,fibTail(3))
+    assertEquals(3,fibTail(4))
+
+    assertEquals(21,fibTail(8))
+  }
 }
