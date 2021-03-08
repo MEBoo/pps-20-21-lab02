22608841c9bd1267353f09ae0324733bf43a6ec0
MEBoo <39593253+MEBoo@users.noreply.github.com>
2021-03-08 22:45:02 +0100

    impl Optional + test

--- src/u02/Optionals.scala
+++ src/u02/Optionals.scala
@@ -21,6 +21,25 @@ object Optionals extends App {
       case Some(a) => f(a)
       case _ => None()
     }
+
+    def filter[A](opt: Option[A])(f:A => Boolean): Option[A] = opt match {
+      case Some(a) if f(a) => opt
+      case _ => None()
+    }
+
+    // non ho capito bene il testo dell'esercizio - se presente trasformare in Some(true) ?
+    def map[A](opt: Option[A])(f:A => Boolean): Option[Boolean] = opt match {
+      case Some(a) if f(a) => Some(true)
+      case _ => None()
+    }
+
+    def map2[A](opt1: Option[A],opt2: Option[A])(f:(A,A) => A): Option[A] = opt1 match {
+      case None() => None();
+      case Some(a) => opt2 match {
+        case None() => None();
+        case Some(b) => Some(f(a,b))
+      }
+    }
   }
 
   import Option._
---
+++ test/u02/OptionalsTest.scala
@@ -0,0 +1,26 @@
+package u02
+
+import org.junit.jupiter.api.Assertions._
+import org.junit.jupiter.api.Test
+
+class OptionsTest {
+
+  import Optionals.Option._
+
+  @Test def testFilter(){
+    assertEquals(Some(5),filter(Some(5))(_ > 2))
+    assertEquals(None(),filter(Some(5))(_ > 8))
+  }
+
+  @Test def testMap(){
+    assertEquals(Some(true),map(Some(5))(_ > 2))
+    assertEquals(None(),map(None[Int])(_ > 2))
+  }
+
+  @Test def testMap2(){
+    assertEquals(Some(15),map2(Some(5),Some(10))(_+_))
+    assertEquals(None(),map2(Some(5),None())(_+_))
+    assertEquals(None(),map2(None(),Some(5))(_+_))
+  }
+  
+}
